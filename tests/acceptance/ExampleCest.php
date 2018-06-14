7db4815e99e8141fc030ac9c18355097736220a2
Allan Paiste <allan.paiste@vaimo.com>
2018-06-14 10:28:42 +0200

    test simplified

--- tests/acceptance/ExampleCest.php
+++ tests/acceptance/ExampleCest.php
@@ -1,15 +1,17 @@
 <?php
+/**
+ * Copyright © Vaimo Group. All rights reserved.
+ * See LICENSE_VAIMO.txt for license details.
+ */
 
 class ExampleCest
 {
-    public function tryUsingGoogle(AcceptanceTester $I)
+    public function tryFillingGoogleSearchInput(AcceptanceTester $I)
     {
         $I->amOnPage('/');
 
-        $I->pressKey('[name="q"]', 'hello');
+        $I->fillField('q', 'hello');
 
-        $text = $I->grabValueFrom('[name="q"]');
-
-        $I->assertEquals('hello', $text);
+        $I->assertEquals('hello', $I->grabValueFrom('q'));
     }
 }
